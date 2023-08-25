e880f7dea18dff9624a8cf8c6176f9b8db6a8607
Danifleikson Buriti Lisboa <danifleikson@hotmail.com>
2023-08-24 21:40:37 -0300

    Aplicando a funcionalidade de calcular desconto extra, utilizando o pattern State.

--- src/main/java/br/com/varejo/loja/orcamento/Orcamento.java
+++ src/main/java/br/com/varejo/loja/orcamento/Orcamento.java
@@ -1,15 +1,50 @@
 package br.com.varejo.loja.orcamento;
 
+import br.com.varejo.loja.orcamento.situacao.Aprovado;
+import br.com.varejo.loja.orcamento.situacao.EmAnalise;
+import br.com.varejo.loja.orcamento.situacao.Finalizado;
+import br.com.varejo.loja.orcamento.situacao.Reprovado;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private SituacaoOrcamento situacao;
+
+    public Orcamento(SituacaoOrcamento situacao) {
+        this.situacao = new EmAnalise();
+    }
+
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDescontoExtra = this.situacao.calcularValordescontoExtra(this);
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+    public void reprovar(){
+        this.situacao.reprovar(this);
+    }
+    public void finalizar(){
+        this.situacao.finalizar(this);
+    }
+
+    public void setSituacao(Finalizado finalizar) {
+    }
+
+
+    public void setSituacao(Aprovado aprovado) {
+    }
+
+    public void setSituacao(Reprovado reprovado) {
+    }
 }
